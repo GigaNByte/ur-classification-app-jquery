94cb9125952fd7ef1a87576b82f8b784f4d1efc5
GigaNByte <karol.saj0@gmail.com>
2022-01-13 02:22:22 +0100

    added comments in jsdoc format+ html refact.

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 
+var imageCounter = 0; //stores globaly counter for creating unique ids for single images
+
+/**
+ * 
+ * @param {*} string 
+ * @returns Capitalized First Letter String
+ */
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 
-
-var imageCounter = 0;
+/**
+ * Entry point for JQuery, function manipulates app dom
+ */
 $(document).ready(function () {
 
+    //hides empty sections on page load 
     $("#app-single-result").hide();
     $("#recent-container").hide();
-    /*extends click event of image-upload to add-image-button element*/
+
+    //extends click event of image-upload to add-image-button element
     $("#add-image-button").on("click", (e) => {
         $("#image-upload").trigger('click');
     });
     /* listens for image upload change*/
     $("#image-upload").on("change", handleImageChange);
 
+    /**
+     * Creates new Image object from url
+     * @param {*} src url of image to be loaded
+     * @returns Promise 
+     */
     const loadMobilenetImage = (src) => {
         return new Promise((resolve, reject) => {
             const img = new Image();
@@ -23,26 +41,33 @@ $(document).ready(function () {
         })
     }
 
-    //This function could be memoized
-    const capitalizeFirstLetter = (string) => {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
 
     var loadMobileNetModel = mobilenet.load();
+
+    /**
+     * Handles the classification, api fetches and upload of image with proper async order after upload button change event
+     * @param {*} e Event 
+     */
     function handleImageChange(e) {
 
         var imgUrl = URL.createObjectURL(e.target.files[0]);
+
         loadMobilenetImage(imgUrl).then(async img => {
+
+            //classifies image
             const predictions = await (await (loadMobileNetModel)).classify(img);
             console.log(predictions);
+
+            //fetch additional info about predicted image based on prediction title
             const wikipediaResponse = await fetch("https://en.wikipedia.org/w/rest.php/v1/search/page?" + new URLSearchParams({
                 limit: 1,
                 q: predictions[0].className.split(',')[0],
             }));
             const wikipediaData = (await wikipediaResponse.json()).pages[0];
             console.log(wikipediaResponse.status);
-            if (wikipediaResponse.status === 200) {
 
+            //if success add data to dom
+            if (wikipediaResponse.status === 200) {
                 $("#app-single-result #result-image").attr('src', imgUrl);
                 $("#app-single-result .app-result-desc").html(wikipediaData.description);
                 $("#app-single-result .app-result-title").html(capitalizeFirstLetter(predictions[0].className.split(',')[0]));
@@ -59,7 +84,6 @@ $(document).ready(function () {
                 $("#recent-gallery-container > #recent-gallery-single-item-" + imageCounter).append($('#app-single-result > .app-box-paper-result').clone());
                 $("#recent-container").show();
             }
-            console.log(imageCounter);
             imageCounter++;
         }).catch(err => console.error(err));
 
